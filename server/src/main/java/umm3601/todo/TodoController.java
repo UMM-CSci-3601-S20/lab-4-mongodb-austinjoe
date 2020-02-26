395c592704c47e1ba12b7badffaf1c48139960fb
Joe Walbran <walbr037@morris.umn.edu>
2020-02-26 13:24:02 -0600

    Implement filtering by body and category
    Making the tests from the previous commit pass.
    
    This is part of issues #1, #5, and #6.

--- server/src/main/java/umm3601/todo/TodoController.java
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -50,6 +50,7 @@ public class TodoController {
     if (ctx.queryParamMap().containsKey("owner")) {
       filters.add(regex("owner", ctx.queryParam("owner"), "i"));
     }
+
     if (ctx.queryParamMap().containsKey("status")) {
       boolean targetStatus = false;
       // We set the target status to false to just save a step, so the target status
@@ -58,6 +59,14 @@ public class TodoController {
         targetStatus = true;
       }
       filters.add(eq("status", targetStatus));
+    }
+
+    if (ctx.queryParamMap().containsKey("body")) {
+      filters.add(regex("body", ctx.queryParam("body"), "i"));
+    }
+
+    if (ctx.queryParamMap().containsKey("category")) {
+      filters.add(regex("category", ctx.queryParam("category"), "i"));
     }
 
     ctx.json(todoCollection.find(filters.isEmpty() ? new Document() : and(filters))
