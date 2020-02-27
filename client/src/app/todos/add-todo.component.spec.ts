3a116a695600ed32be11b1e0651902cc37f77593
Joe Walbran <walbr037@morris.umn.edu>
2020-02-27 15:52:52 -0600

    Add spec for AddTodoComponent
    It has two (very small) tests inside it at the moment. Both are passing.

---
+++ client/src/app/todos/add-todo.component.spec.ts
@@ -0,0 +1,60 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, NgForm, ReactiveFormsModule, FormGroup, AbstractControl } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { By } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockTodoService } from 'src/testing/todo.service.mock';
+import { AddTodoComponent } from './add-todo.component';
+import { TodoService } from './todo.service';
+
+describe('AddTodoComponent', () => {
+  let addTodoComponent: AddTodoComponent;
+  let addTodoForm: FormGroup;
+  let calledClose: boolean;
+  let fixture: ComponentFixture<AddTodoComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        MatSnackBarModule,
+        MatCardModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatInputModule,
+        BrowserAnimationsModule,
+        RouterTestingModule
+      ],
+      declarations: [AddTodoComponent],
+      providers: [{ provide: TodoService, useValue: new MockTodoService() }]
+    }).compileComponents().catch(error => {
+      expect(error).toBeNull();
+    });
+  }));
+
+  beforeEach(() => {
+    calledClose = false;
+    fixture = TestBed.createComponent(AddTodoComponent);
+    addTodoComponent = fixture.componentInstance;
+    addTodoComponent.ngOnInit();
+    fixture.detectChanges();
+    addTodoForm = addTodoComponent.addTodoForm;
+    expect(addTodoForm).toBeDefined();
+    expect(addTodoForm.controls).toBeDefined();
+  });
+
+  it('should create the component and form', () => {
+    expect(addTodoComponent).toBeTruthy();
+    expect(addTodoForm).toBeTruthy();
+  });
+
+  it('form should be invalid when empty', () => {
+    expect(addTodoForm.valid).toBeFalsy();
+  });
+});
