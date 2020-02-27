43670c6cc00e80580e1b38cedda80545e2d4d0a1
Joe Walbran <walbr037@morris.umn.edu>
2020-02-27 16:27:13 -0600

    Test the category and body fields
    These tests—which are very similar to the user tests—are also currently passing.

--- client/src/app/todos/add-todo.component.spec.ts
+++ client/src/app/todos/add-todo.component.spec.ts
@@ -89,4 +89,71 @@ describe('AddTodoComponent', () => {
       expect(ownerControl.hasError('maxlength')).toBeTruthy();
     });
   });
+
+  describe('The category field', () => {
+    let categoryControl: AbstractControl;
+
+    beforeEach(() => {
+      categoryControl = addTodoComponent.addTodoForm.controls[`category`];
+    });
+
+    it('should not allow empty categories', () => {
+      categoryControl.setValue('');
+      expect(categoryControl.valid).toBeFalsy();
+    });
+
+    it('should be fine with "video games"', () => {
+      categoryControl.setValue('video games');
+      expect(categoryControl.valid).toBeTruthy();
+    });
+
+    it('should fail on single character categories', () => {
+      categoryControl.setValue('x');
+      expect(categoryControl.valid).toBeFalsy();
+      expect(categoryControl.hasError('minlength')).toBeTruthy();
+    });
+
+    it('should fail on really long categories', () => {
+      categoryControl.setValue('x'.repeat(100));
+      expect(categoryControl.valid).toBeFalsy();
+      expect(categoryControl.hasError('maxlength')).toBeTruthy();
+    });
+  });
+
+  describe('The body field', () => {
+    let bodyControl: AbstractControl;
+
+    beforeEach(() => {
+      bodyControl = addTodoComponent.addTodoForm.controls[`body`];
+    });
+
+    it('should not allow empty bodies', () => {
+      bodyControl.setValue('');
+      expect(bodyControl.valid).toBeFalsy();
+    });
+
+    it('should be fine with "Clean your room"', () => {
+      bodyControl.setValue('Clean your room');
+      expect(bodyControl.valid).toBeTruthy();
+    });
+
+    it('should fail on single character bodies', () => {
+      bodyControl.setValue('x');
+      expect(bodyControl.valid).toBeFalsy();
+      expect(bodyControl.hasError('minlength')).toBeTruthy();
+    });
+
+    it('should fail on really long bodies', () => {
+      bodyControl.setValue('x'.repeat(2000));
+      expect(bodyControl.valid).toBeFalsy();
+      expect(bodyControl.hasError('maxlength')).toBeTruthy();
+    });
+
+    it('should accept medium-length bodies, though', () => {
+      bodyControl.setValue('x'.repeat(550));
+      expect(bodyControl.valid).toBeTruthy();
+      expect(bodyControl.hasError('maxlength')).toBeFalsy();
+    });
+
+  });
 });
